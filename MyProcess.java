e1b87baad5ec1a2689612e7c89047cbb00f2f623
MinnehWanjohi <minnehwanjohi@gmail.com>
2026-01-11 01:09:29 +0300

    Add files via upload

---
+++ MyProcess.java
@@ -0,0 +1,8 @@
+public class MyProcess {
+public static void main(String[] args) {
+
+double celsius = 30.0;
+double fahrenheit = (celsius * 9/5) + 32;
+System.out.println(celsius + "°C = " +  fahrenheit + "°F");
+}
+}
