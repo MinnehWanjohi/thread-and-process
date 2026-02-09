1f20a62cb72438fe400c7d8b41bb320d350eec1f
MinnehWanjohi <minnehwanjohi@gmail.com>
2026-02-09 10:56:23 +0300

    Refactor Dog and Puppy classes
    Removed static printInfo methods and simplified output.

--- Animals.java
+++ Animals.java
@@ -1,29 +1,23 @@
 class Dog{
 public void bark(){
-System.out.println("Dog make a sound - barking...");
-}
-public static void printInfo() {
-System.out.println("Dog is barking...");
+System.out.println("Dog make a sound - barking");
+
 }
 }
 class Puppy extends Dog{
 @Override
 public void bark(){
-System.out.println("Puppy barks..." );
-}
-public static void printInfo() {
-System.out.println("puppy is barking");
+System.out.println("Puppy barks" );
 }
 }
 public class Animals {
 public static void main(String[] args) {
 Dog myDog = new Puppy();
-System.out.println("The puppy is barking \n");
 
-myDog.bark();       // Dynamic binding — calls Puppy’s bark()
-myDog.printInfo();  // Static binding — calls Dog’s printInfo() 
+myDog.bark(); 
 
 }
 }
+
 
 
